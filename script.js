b67c8766efc7fb407f406dce365b1e668a609945
Ben Lewis <104620277+benLewisDev@users.noreply.github.com>
2023-03-24 13:25:04 +1030

    add sub menu and thank you message

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Rotator/picture slider
+
 let slideIndex = 0;
 showSlides();
 
@@ -45,3 +47,49 @@ slideRight.addEventListener("click", function () {
 });
 
 
+// Hamburger Menu
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const subMenu = document.querySelector('.sub-menu');
+
+// Show the sub-menu on hover of the hamburger menu
+hamburgerMenu.addEventListener('mouseover', function() {
+  subMenu.classList.add('show-menu');
+});
+
+// Hide the sub-menu when the user clicks anywhere else on the page
+document.addEventListener('click', function(event) {
+  if (!hamburgerMenu.contains(event.target) && !subMenu.contains(event.target)) {
+    subMenu.classList.remove('show-menu');
+  }
+});
+
+
+//Thank you message
+const contactForm = document.getElementById("contactForm");
+const messageContainer = document.getElementById("messageContainer");
+
+contactForm.addEventListener("submit", function (event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get the values of the form inputs
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const phoneNumber = document.getElementById("phoneNumber").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  // Hide the form
+  contactForm.style.display = "none";
+
+  // Construct the personalized message
+  const personalizedMessage = `
+    <h2>Thank you, ${firstName}!</h2>
+    <p>We have received your message and will get back to you as soon as possible.</p>`
+    ;
+
+  // Display the personalized message
+  messageContainer.innerHTML = personalizedMessage;
+  messageContainer.style.display = "block";
+});
